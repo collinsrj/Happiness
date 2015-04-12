db6cd1c81cefe01156f3cb03dec2196694632327
Robert Collins <collinsrj@gmail.com>
2015-04-12 10:39:58 +0100

    Implemented the happiness view. Added the annotation to make the face appear in the Storyboard.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Robert Collins on 11/04/2015.
+//  Copyright (c) 2015 Robert Collins. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
